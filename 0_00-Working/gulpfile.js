9f89b87cda7e687063938e9c115fdae8f491d53b
Kagerjay <acpython777@gmail.com>
2017-09-03 00:39:10 -0400

    gulp coffee pipes

--- 0_00-Working/gulpfile.js
+++ 0_00-Working/gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp');
     gutil = require('gulp-util');
+    coffee = require('gulp-coffee');
 
 gulp.task('log', function(){
   gutil.log('Workflows are awesome');
+
+});
+
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee',function(){
+  gulp.src(coffeeSources)
+    .pipe(coffee({ bare: true}) //pipe it through coffee bare plugin
+      .on('error', gutil.log)) //sends error if invalid coffeescript, and log in console
+    .pipe(gulp.dest('components/scripts')) //push results to scripts folder
 });

eabd99e6b74ec2f653929da349f95598c73a1e15
Kagerjay <acpython777@gmail.com>
2017-09-04 01:17:03 -0400

    revisit later, comments didn't work with gulp compass

--- 0_00-Working/gulpfile.js
+++ 0_00-Working/gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
     gutil = require('gulp-util');
     coffee = require('gulp-coffee');
     browserify = require('gulp-browserify');
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 gulp.task('log', function(){
@@ -17,6 +18,7 @@ var jsSources = [
   'components/scripts/tagline.js',
   'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss'];
 
 //Processes raw coffee file into tagline.js (referenced in coffee file)
 gulp.task('coffee',function(){
@@ -32,4 +34,16 @@ gulp.task('js',function(){
     .pipe(concat('script.js'))  //one file that we will reference in index.html
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js')) //final destination
+});
+
+//Sass process data
+gulp.task('compass',function(){
+  gulp.src(sassSources)
+    .pipe(compass({  //compass library
+      sass: 'components/sass', //directory
+      image: 'builds/development/images',
+      stye: 'expanded'
+    }))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('builds/development/css')) //final destination
 });
